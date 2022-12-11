647822359c2daf6dad44133cd5c5efa0a0b0bc59
victorhugo-js <vt.hugo.2021@gmail.com>
2022-12-11 15:57:34 -0300

    finalizado projeto crud com MVC

---
+++ Models/Contato.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Telefone { get; set; }
+        public bool Ativo { get; set; }
+        
+        
+        
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+using MVC.Context;
+using System.Collections.Immutable;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AgendaContext>(options =>
+ options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoDB")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
